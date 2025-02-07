d607afc4ec16538cb9239380104af394cce01d1f
vetlesho <vetlesho@stud.ntnu.no>
2025-02-07 12:24:11 +0100

    create folder for task 3

---
+++ calculator-3/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Calculator,
+  },
+};
+</script>
